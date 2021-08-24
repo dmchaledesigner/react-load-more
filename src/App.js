df08c36b3080e7a19337e625b816cd18f9b23799
david mchale <dmchaledesigner@gmail.com>
2021-08-25 08:04:37 +1000

    3.final

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import styled from 'styled-components/macro';
-import Heading from './Heading';
 import PostsBlock from './PostsBlock';
 
 
 function App() {
   return (
     <AppStyled className="container-fluid">
-      <Heading />
       <PostsBlock />
     </AppStyled>
   );
@@ -15,8 +13,7 @@ function App() {
 
 
 const AppStyled = styled.div`
-  padding: 4rem 0rem;
-  background-color: #f8f8f8;
+  padding: 4rem 0rem
 `
 
 export default App;
